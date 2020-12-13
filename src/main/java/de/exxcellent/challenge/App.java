2c4b1c7920a5635b6cf96676fe8f3e9806566159
Heiko <Heiko.Fackler@web.de>
2020-12-13 16:10:04 +0100

    solve 1) task

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,12 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.TableFileReader.CSVFileReader;
+import de.exxcellent.challenge.Util.ListUtil;
+import de.exxcellent.challenge.WeatherData.DailyWeatherData;
+import de.exxcellent.challenge.WeatherData.WeatherDataFactory;
+
+import java.util.List;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -16,8 +23,12 @@ public final class App {
 
         // Your preparation code …
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+        List<String[]> data = CSVFileReader.readCSV("src/main/resources/de/exxcellent/challenge/weather.csv", true);
+        List<DailyWeatherData> weatherData = WeatherDataFactory.createWeatherData(data);
+        DailyWeatherData smallestTemperatureSpread =
+                ListUtil.findMinimum(weatherData, dWD -> dWD.getMaxTemperature() - dWD.getMinTemperature());
+
+        System.out.printf("Day with smallest temperature spread : %s%n", smallestTemperatureSpread.getDay());
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
