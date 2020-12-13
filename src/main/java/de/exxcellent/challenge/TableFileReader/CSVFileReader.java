38f441e050323b65b5a09dd4f3e56ba8e89af1dd
Heiko <Heiko.Fackler@web.de>
2020-12-13 14:50:57 +0100

    implement skip header data

--- src/main/java/de/exxcellent/challenge/TableFileReader/CSVFileReader.java
+++ src/main/java/de/exxcellent/challenge/TableFileReader/CSVFileReader.java
@@ -16,10 +16,15 @@ public class CSVFileReader {
     }
 
     public static List<String[]> readCSV(String path) {
+        return readCSV(path, false);
+    }
+
+    public static List<String[]> readCSV(String path, boolean skipHeader) {
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             return br
                     .lines()
+                    .skip(skipHeader ? 1 : 0)
                     .map(line -> line.split(DELIMITER_REGEX))
                     .collect(Collectors.toList());
         } catch (FileNotFoundException e) {
