f2fc6a97ca66c51036cd3651d71ef00d9e3919e9
Heiko <Heiko.Fackler@web.de>
2020-12-13 14:18:18 +0100

    base structure

---
+++ src/main/java/de/exxcellent/challenge/Util/ListUtil.java
@@ -0,0 +1,4 @@
+package de.exxcellent.challenge.Util;
+
+public class ListUtil {
+}
---
+++ src/main/java/de/exxcellent/challenge/WeatherData/DailyWeatherData.java
@@ -0,0 +1,7 @@
+package de.exxcellent.challenge.WeatherData;
+
+public class DailyWeatherData {
+    class Builder {
+        
+    }
+}
