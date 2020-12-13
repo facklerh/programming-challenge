09b8dd00c58176b2c8097324d77039a5adf6a84f
Heiko <Heiko.Fackler@web.de>
2020-12-13 16:09:40 +0100

    implement list util min function

--- src/main/java/de/exxcellent/challenge/Util/ListUtil.java
+++ src/main/java/de/exxcellent/challenge/Util/ListUtil.java
@@ -1,4 +1,13 @@
 package de.exxcellent.challenge.Util;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
 public class ListUtil {
+    public static <T> T findMinimum(List<T> testData, Function<T, Double> valueOf) {
+        Optional<T> min = testData.stream().min(Comparator.comparingDouble(valueOf::apply));
+        return min.orElse(null);
+    }
 }
