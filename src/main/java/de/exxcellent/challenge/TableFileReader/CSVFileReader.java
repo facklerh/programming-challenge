07af71a3fba4f74c301016b6ab6119687a21cacc
Heiko <Heiko.Fackler@web.de>
2020-12-13 14:48:02 +0100

    implement csv reader

--- src/main/java/de/exxcellent/challenge/TableFileReader/CSVFileReader.java
+++ src/main/java/de/exxcellent/challenge/TableFileReader/CSVFileReader.java
@@ -1,4 +1,30 @@
 package de.exxcellent.challenge.TableFileReader;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CSVFileReader {
+
+    public static final String DELIMITER_REGEX = "([,;])";
+
+    public CSVFileReader(String s) {
+
+    }
+
+    public static List<String[]> readCSV(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            return br
+                    .lines()
+                    .map(line -> line.split(DELIMITER_REGEX))
+                    .collect(Collectors.toList());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
 }
