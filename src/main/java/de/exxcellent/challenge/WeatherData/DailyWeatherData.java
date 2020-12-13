57643091d0cd7a0f2603f3508ca215f9dbf23756
Heiko <Heiko.Fackler@web.de>
2020-12-13 15:38:20 +0100

    create data class

--- src/main/java/de/exxcellent/challenge/WeatherData/DailyWeatherData.java
+++ src/main/java/de/exxcellent/challenge/WeatherData/DailyWeatherData.java
@@ -1,7 +1,50 @@
 package de.exxcellent.challenge.WeatherData;
 
 public class DailyWeatherData {
-    class Builder {
-        
+    private int day;
+    private double minTemperature;
+    private double maxTemperature;
+
+    private DailyWeatherData(DailyWeatherData.Builder builder) {
+        this.day = builder.day;
+        this.minTemperature = builder.minTemperature;
+        this.maxTemperature = builder.maxTemperature;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public double getMinTemperature() {
+        return minTemperature;
+    }
+
+    public double getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    static class Builder {
+        private int day;
+        private double minTemperature;
+        private double maxTemperature;
+
+        public Builder withDay(int day) {
+            this.day = day;
+            return this;
+        }
+
+        public Builder withMinTemperature(double minTemperature) {
+            this.minTemperature = minTemperature;
+            return this;
+        }
+
+        public Builder withMaxTemperature(double maxTemperature) {
+            this.maxTemperature = maxTemperature;
+            return this;
+        }
+
+        public DailyWeatherData build() {
+            return new DailyWeatherData(this);
+        }
     }
 }
