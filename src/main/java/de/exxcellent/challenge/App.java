3fea1de5d61fae2702edbea7097e67c76f91e229
Heiko <Heiko.Fackler@web.de>
2020-12-13 16:44:45 +0100

    solve second solution

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,9 +1,11 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.Data.Football.FootballDataFactory;
+import de.exxcellent.challenge.Data.Football.FootballTeamData;
+import de.exxcellent.challenge.Data.Weather.DailyWeatherData;
+import de.exxcellent.challenge.Data.Weather.WeatherDataFactory;
 import de.exxcellent.challenge.TableFileReader.CSVFileReader;
 import de.exxcellent.challenge.Util.ListUtil;
-import de.exxcellent.challenge.WeatherData.DailyWeatherData;
-import de.exxcellent.challenge.WeatherData.WeatherDataFactory;
 
 import java.util.List;
 
@@ -23,14 +25,21 @@ public final class App {
 
         // Your preparation code …
 
-        List<String[]> data = CSVFileReader.readCSV("src/main/resources/de/exxcellent/challenge/weather.csv", true);
-        List<DailyWeatherData> weatherData = WeatherDataFactory.createWeatherData(data);
+        List<String[]> rawWeatherData = CSVFileReader.readCSV("src/main/resources/de/exxcellent/challenge/weather.csv", true);
+        List<DailyWeatherData> weatherData = WeatherDataFactory.createWeatherData(rawWeatherData);
         DailyWeatherData smallestTemperatureSpread =
                 ListUtil.findMinimum(weatherData, dWD -> dWD.getMaxTemperature() - dWD.getMinTemperature());
 
         System.out.printf("Day with smallest temperature spread : %s%n", smallestTemperatureSpread.getDay());
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+        List<String[]> rawFootballData = CSVFileReader.readCSV("src/main/resources/de/exxcellent/challenge/football.csv", true);
+        List<FootballTeamData> footballTeamData = FootballDataFactory.createFootballTeamData(rawFootballData);
+        FootballTeamData smallestDifference = ListUtil.findMinimum(footballTeamData, fTD ->
+                Double.valueOf(fTD.getGoals() > fTD.getGoalsAllowed()
+                        ? fTD.getGoals() - fTD.getGoalsAllowed()
+                        : fTD.getGoalsAllowed() - fTD.getGoals())
+        );
+
+        System.out.printf("Team with smallest goal spread       : %s%n", smallestDifference.getName());
     }
 }
